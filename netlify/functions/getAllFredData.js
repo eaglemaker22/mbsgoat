b9642fa78d5b1cf61c63623d624105066e9a016e
eaglemaker22 <cstheall@yahoo.com>
2025-06-10 14:01:16 -0700

    Update getAllFredData.js

--- netlify/functions/getAllFredData.js
+++ netlify/functions/getAllFredData.js
@@ -1,9 +1,12 @@
-{
-  "Retail Sales (Excl. Food)": {
-    "latest": 743.2,
-    "last_month": 732.8,
-    "year_ago": 690.4,
-    "latest_date": "2025-06-17"
-  },
-  ...
+return {
+  statusCode: 200,
+  body: JSON.stringify({
+    "Retail Sales (Excl. Food)": {
+      latest: "0.2%",
+      last_month: "0.1%",
+      year_ago: "1.5%",
+      latest_date: "2025-06-01"
+    },
+    ...
+  })
 }
