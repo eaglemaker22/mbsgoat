1c59dd7f52752bbe404e352075bacfeaa9bcc116
eaglemaker22 <cstheall@yahoo.com>
2025-07-16 11:55:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -208,7 +208,7 @@ async function fetchAndUpdateDailyRates() {
       }
 
       updateTextElement(`${prefix}LastMonth`, formatPercentage(rateData.last_month));
-      updateTextElement(`${prefix}YearAgo`, formatPercentage(rateData.year_ago)); // Corrected missing ')'
+      updateTextElement(`${prefix}YearAgo`, formatPercentage(rateData.year_ago)); 
 
       let changeVs1M = null;
       let changeVs1Y = null;
